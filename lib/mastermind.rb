17461ffc9047cee10d7f3386518fe039628fa2cb
zamrivertech <mohamadtasul8000@gmail.com>
2026-07-24 10:06:14 +0200

    Import colorize, rubocop and add and connect blank files and classes for mastermind game

---
+++ lib/mastermind.rb
@@ -0,0 +1,5 @@
+require_relative 'mastermind/display'
+require_relative 'mastermind/player'
+require_relative 'mastermind/board'
+require_relative 'mastermind/peg'
+require_relative 'mastermind/game'
---
+++ lib/mastermind/display.rb
@@ -0,0 +1,4 @@
+require 'colorize'
+
+class Display
+end
