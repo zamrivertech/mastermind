35058c73abd92ccab61210e418c0ed5ba1377af9
zamrivertech <mohamadtasul8000@gmail.com>
2026-07-29 13:37:45 +0200

    Implement computer player to make secret code if role is code maker

--- lib/mastermind/computer_player.rb
+++ lib/mastermind/computer_player.rb
@@ -1,6 +1,13 @@
+# ComputerPlayer Class: act as code maker or breaker opposite as human player
 class ComputerPlayer
   include Player
   def initialize(human_role)
     @role = human_role == @@roles[0] ? @@roles[1] : @@roles[0]
+  end
+
+  def make_code
+    return unless codemaker?
+
+    [Peg.color(rand(0..3)), Peg.color(rand(0..3)), Peg.color(rand(0..3)), Peg.color(rand(0..3))]
   end
 end
--- lib/mastermind/game.rb
+++ lib/mastermind/game.rb
@@ -4,6 +4,9 @@ class Game < Interaction
   human_player = HumanPlayer.new(maker_or_breaker)
   computer_player = ComputerPlayer.new(human_player.role?)
   # announce_roles(human_player, computer_player)
-  board = Board.new(4, 4)
-  human_player.add_color(board)
+  # computer needs to choose random 4 colors
+  # board = Board.new(4, 4)
+  # human_player.add_color(board)
+  # display_board(board)
+  p computer_player.make_code
 end
