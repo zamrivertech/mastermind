08719442ac52e9f18560b49718a9f038ab2e511d
zamrivertech <mohamadtasul8000@gmail.com>
2026-08-04 07:29:27 +0200

    Move add_color to human class

--- lib/mastermind/human_player.rb
+++ lib/mastermind/human_player.rb
@@ -1,4 +1,20 @@
 # handle human player logic
 class HumanPlayer
   include Player
+
+  # add color as human code breaker into board color pegs
+  def add_color
+    return unless codebreaker?
+
+    while Board.current_color_row_index != -1
+      user_choice = Input.get_peg_color.split('')
+
+      Board.add_color(Peg.color(user_choice[0]), Board.current_color_row_index, user_choice[1].to_i - 1)
+      # if coderbeaker is sure and would like to move on...
+      # ...to next row, confirm yes or no
+      # if yes, close current row, get key peg result...
+      # and move on, if not
+      # delete last added color or delete all, add this options
+    end
+  end
 end
